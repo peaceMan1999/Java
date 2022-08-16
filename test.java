16c99565f573ff16f83f499a4d9cc64451645652
流年玄月 <984014088@qq.com>
2022-08-16 17:50:38 +0800

    java初次练习--变量

---
+++ test.java
@@ -0,0 +1,42 @@
+package hello;
+
+public class test {
+    public static void main(String[] args) {
+        int age = 10;
+        System.out.println(age);
+        age += 10;
+        System.out.println(age);
+        int b = 1000000000;
+        long c = 1000000000000L;
+        float f = 10.10f;
+        boolean flag = true;
+        String name = "hehe";
+        char aa = 'a';
+
+        char q = 10;
+        char w = 255;
+        int v = q + w;
+        int ww = 11;
+        byte qq = (byte) ww;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
